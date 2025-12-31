67907a959b123cacf87e70af2928a12ebf79b031
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2025-12-31 19:33:24 +0530

    feat: implement routing and authentication in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import AuthBootstrap from "./auth/AuthBootstrap"
+import Dashboard from "./pages/Dashboard"
+import LandingPage from "./pages/LandingPage"
+import LearningSession from "./pages/LearningSession"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="text-3xl ">App</div>
+    <>
+    <AuthBootstrap />
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/notebooks" element={<Dashboard />} />
+      <Route path="/learning-session" element={<LearningSession />} />
+    </Routes>
+    </>
   )
 }
 
