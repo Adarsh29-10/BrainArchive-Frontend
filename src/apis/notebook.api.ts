f53e9e5a1b9f74026b410a39c17c238b74d16429
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-08 23:34:29 +0530

    feat: enhance notebook API with updated create and update functions

--- src/apis/notebook.api.ts
+++ src/apis/notebook.api.ts
@@ -1,5 +1,7 @@
 import { api } from "./axios.api";
 import type { Notebook } from "../types/notebook";
+import type { Block } from "../types/block";
+
 
 export const getNotebooks = async (): Promise<Notebook[]> => {
     const res = await api.get('/notebooks');
@@ -7,20 +9,24 @@ export const getNotebooks = async (): Promise<Notebook[]> => {
 }
 
 export const createNotebook = async (data: {
-    title: string,
-    description?: string
+    title: string;
+    description?: string;
+    blocks: [];
+    isPublic: boolean;
+    lastActivityAt: Date;
+    totalTimeSpent: number;
 }) => {
     const res = await api.post('/notebooks', data);
     return res.data.data;
 }
 
 export const updateNotebook = async (data: {
-    id: string,
+    notebookId: string,
     title: string,
     description?: string,
 }) => {
-    const {id, ...payload} = data;
-    const res = await api.put(`/notebooks/${id}`, payload);
+    const {notebookId, ...payload} = data;
+    const res = await api.put(`/notebooks/${notebookId}`, payload);
     return res.data.data;
 }
 
@@ -28,3 +34,21 @@ export const deleteNotebook = async (id:string) => {
     const res = await api.delete(`/notebooks/${id}`);
     return res.data.data;
 }
+
+export const getNotebookById = async (notebookId: string) => {
+    const res = await api.get(`/notebooks/${notebookId}`);
+    return res.data.data;
+}
+
+export const updateNotebookBlock = async (params: {
+  notebookId: string;
+  blocks: Block[];
+}) => {
+  const { notebookId, blocks } = params;
+
+  const res = await api.patch(`/notebooks/${notebookId}/blocks`, {
+    blocks,
+  });
+
+  return res.data.data;
+};
