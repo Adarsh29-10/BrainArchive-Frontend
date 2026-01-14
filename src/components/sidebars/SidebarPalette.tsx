2e3ba73a139bb781ea7fbfd02aa65f2f78720a71
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-14 23:46:23 +0530

    feat: add SidebarPalette component with predefined sections and blocks

---
+++ src/components/sidebars/SidebarPalette.tsx
@@ -0,0 +1,29 @@
+import { Code2, FileText, Heading, Heading1, List } from "lucide-react";
+
+export const SIDEBAR_SECTIONS = [
+    {
+        title: 'Headings',
+        blocks: [
+            { type: 'heading', label: 'Heading', Icon: Heading },
+            { type: 'heading1', label: 'Heading 1', Icon: Heading1 },
+        ] 
+    },
+    {
+        title: 'Content',
+        blocks: [
+            { type: 'paragraph', label: 'Paragraph', Icon: FileText },
+        ] 
+    },
+    {
+        title: 'Lists',
+        blocks: [
+            { type: 'bullet', label: 'Bullet Points', Icon: List },
+        ] 
+    },
+    {
+        title: 'Code',
+        blocks: [
+            { type: 'code', label: 'Code Block', Icon: Code2 },
+        ] 
+    },
+]
