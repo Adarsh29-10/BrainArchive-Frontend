7292c8decd316e0c9715c18fd7f5a2fe0afa7d90
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-15 23:31:54 +0530

    feat: pass autoFocus prop to block components in BlockRenderer for improved focus management

--- src/components/blocks/BlockRenderer.tsx
+++ src/components/blocks/BlockRenderer.tsx
@@ -9,24 +9,25 @@ interface BlockRendererProps {
   block: Block;
   onChange: (id: string, value: string) => void;
   onDelete?: (id: string) => void;
+  autoFocus: string;
 }
 
-function BlockRenderer({ block, onChange, onDelete }: BlockRendererProps) {
+function BlockRenderer({ block, onChange, onDelete, autoFocus }: BlockRendererProps) {
   switch (block.type) {
     case "heading":
-      return <HeadingBlock block={block} onChange={onChange} onDelete={onDelete} />;
+      return <HeadingBlock block={block} onChange={onChange} onDelete={onDelete} autoFocus={autoFocus} />;
 
     case "heading1":
-      return <Heading1Block block={block} onChange={onChange} onDelete={onDelete} />;
+      return <Heading1Block block={block} onChange={onChange} onDelete={onDelete} autoFocus={autoFocus} />;
 
     case "paragraph":
-      return <ParagraphBlock block={block} onChange={onChange} onDelete={onDelete}/>;
+      return <ParagraphBlock block={block} onChange={onChange} onDelete={onDelete} autoFocus={autoFocus} />;
 
     case "bullet":
-        return <BulletBlock block={block} onChange={onChange} onDelete={onDelete}/>;
+      return <BulletBlock block={block} onChange={onChange} onDelete={onDelete} autoFocus={autoFocus} />;
 
     case "code":
-        return <CodeBlock block={block} onChange={onChange} onDelete={onDelete}/>;
+      return <CodeBlock block={block} onChange={onChange} onDelete={onDelete} autoFocus={autoFocus} />;
 
     default:
       return null;
