d9616f948fd54f5e28946017d17b0af63d4efe90
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-17 01:57:42 +0530

    refactor: simplify Navbar component by replacing menu with profile

--- src/components/navbars/Navbar.tsx
+++ src/components/navbars/Navbar.tsx
@@ -1,13 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Menu, X } from "lucide-react";
-import { useState } from "react";
-import SignInButton from "../buttons/SignInButton";
-import LogoutButton from "../buttons/LogoutButton";
+import { useNavigate } from "react-router-dom";
 import { AuthLoadingState } from "../loaders/LoaderStates";
 
 function Navbar() {
-  const { isAuthenticated, user, isLoading } = useAuth0();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { user, isLoading } = useAuth0();
+
+  const navigate = useNavigate()
 
   if (isLoading) {
     return <AuthLoadingState fullScreen={false} />;
@@ -23,48 +21,16 @@ function Navbar() {
           <span className="text-xl sm:text-2xl font-bold text-white">BrainArchive</span>
         </div>
 
-        {/* Desktop Menu */}
-        <div className="hidden md:flex items-center gap-4">
-          {isAuthenticated ? (
-            <>
-              <span className="text-white font-medium">{user?.name}</span>
-              <LogoutButton />
-            </>
-          ) : (
-            <SignInButton />
-          )}
-        </div>
-
-        {/* Mobile Menu Button */}
-        <button
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className="md:hidden p-2 rounded-lg transition-colors"
-          aria-label="Toggle menu"
-        >
-          {isMenuOpen ? (
-            <X size={24} className="text-white" />
-          ) : (
-            <Menu size={24} className="text-white" />
-          )}
-        </button>
+        <img
+          onClick={()=>navigate('/profile')}
+          src={user?.picture}
+          alt={user?.name}
+          className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border-4 border-zinc-900 bg-zinc-800 object-cover"
+        />
+       
       </div>
 
-      {/* Mobile Menu */}
-      {isMenuOpen && (
-        <div className="md:hidden bg-zinc-900  backdrop-blur-md border-t  px-4 py-4 space-y-3">
-          {isAuthenticated ? (
-            <div className="flex flex-col">
-
-              <div className="px-4 py-2 text-white font-medium">
-                Hii, {user?.name}
-              </div>
-              <LogoutButton />
-            </div>
-          ) : (
-            <SignInButton />
-          )}
-        </div>
-      )}
+     
     </nav>
   );
 }
