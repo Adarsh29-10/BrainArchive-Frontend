903a55c25c72494543a55f6cce0166ce35920f7a
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-17 14:33:31 +0530

    style: update NavbarLandingPage background and logo text color for improved visibility

--- src/components/navbars/NavbarLandingPage.tsx
+++ src/components/navbars/NavbarLandingPage.tsx
@@ -11,12 +11,12 @@ function NavbarLandingPage() {
   }
 
   return (
-    <nav className="bg-white/10 backdrop-blur-md border-b border-black/20 sticky top-0 z-50">
+    <nav className="bg-zinc-900/10 backdrop-blur-md border-b border-black/20 sticky top-0 z-50">
       <div className="flex justify-between items-center px-4 sm:px-8 lg:px-16 py-3">
         
         {/* Logo */}
         <div className="flex items-center gap-2">
-          <span className="text-xl sm:text-2xl font-bold text-black">BrainArchive</span>
+          <span className="text-xl sm:text-2xl font-bold text-white">BrainArchive</span>
         </div>
 
         <div className=" md:flex items-center gap-4">
