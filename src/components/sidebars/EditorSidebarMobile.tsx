c102180686a3276f139de1982b292b6b19cf60d0
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-18 22:00:39 +0530

    feat: enhance EditorSidebarMobile with notebookId handling and save functionality

--- src/components/sidebars/EditorSidebarMobile.tsx
+++ src/components/sidebars/EditorSidebarMobile.tsx
@@ -2,32 +2,60 @@ import { useState } from 'react';
 import { SIDEBAR_SECTIONS } from './SidebarPalette';
 import type { BlockType } from '../../types/block';
 import { X } from 'lucide-react';
+import { useUpdateNotebookBlock } from '../../hooks/useNotebooks';
+import { useParams } from 'react-router-dom';
+import { useEditorStore } from '../../hooks/useEditorStore';
 
 type Props = {
   addBlock: (type: BlockType) => void;
 };
 
 export const EditorSidebarMobile = ({ addBlock }: Props) => {
+  const { notebookId } = useParams<{ notebookId: string | undefined}>();
   const [activeSection, setActiveSection] = useState<number | null>(null);
+  const {blocks, isPending} = useEditorStore(notebookId);
+  const updateBlockMutation = useUpdateNotebookBlock();
+  const handleSaveBlocks = () => {
+    if (!notebookId) return;
+
+    updateBlockMutation.mutate({
+      notebookId,
+      blocks
+    });
+  }
 
   return (
     <>
       {/* Minimal Bottom Toolbar */}
       <div className="fixed top-15 left-0 right-0 z-40 bg-zinc-900 border-y border-zinc-500 py-2">
-        <div className="flex items-center justify-start overflow-x-auto scrollbar-hide">
-          {SIDEBAR_SECTIONS.map((section, index) => (
-            <button
-              key={section.title}
-              onClick={() => setActiveSection(activeSection === index ? null : index)}
-              className={`px-3 py-2 text-xs whitespace-nowrap border-r border-zinc-500 last:border-r-0 transition-colors ${
-                activeSection === index
-                  ? 'text-zinc-100 bg-zinc-800'
-                  : 'text-zinc-300 font-semibold hover:text-zinc-100 hover:bg-zinc-800'
+        <div className='flex items-center justify-between pl-2 pr-4'>
+          <div className="flex items-center justify-start overflow-x-auto scrollbar-hide">
+            {SIDEBAR_SECTIONS.map((section, index) => (
+              <button
+                key={section.title}
+                onClick={() => setActiveSection(activeSection === index ? null : index)}
+                className={`px-3 py-2 text-xs whitespace-nowrap border-r border-zinc-500 last:border-r-0 transition-colors ${
+                  activeSection === index
+                    ? 'text-zinc-100 bg-zinc-800'
+                    : 'text-zinc-300 font-semibold hover:text-zinc-100 hover:bg-zinc-800'
+                }`}
+              >
+                {section.title}
+              </button>
+            ))}
+          </div>
+
+          <button
+            onClick={handleSaveBlocks}
+            disabled={isPending || updateBlockMutation.isPending || blocks.length === 0}
+            className={`px-4 py-1 rounded-lg font-semibold
+              ${isPending || updateBlockMutation.isPending || blocks.length === 0
+                ? "bg-gray-400 cursor-not-allowed"
+                : "bg-blue-500 text-white"
               }`}
-            >
-              {section.title}
-            </button>
-          ))}
+          >
+            {isPending ? "Loading..." : updateBlockMutation.isPending ? "Saving..." : "Save"}
+          </button>
         </div>
       </div>
 
@@ -64,6 +92,7 @@ export const EditorSidebarMobile = ({ addBlock }: Props) => {
                     <block.Icon size={24} className="text-white" />
                   </button>
                 ))}
+
               </div>
             
           </div>
