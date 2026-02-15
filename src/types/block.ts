b47854546e4559241cecf9b641e0eea59d069f85
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-02-15 18:09:26 +0530

    feat(BlockType): add 'divider' type to BlockType for enhanced block options

--- src/types/block.ts
+++ src/types/block.ts
@@ -1,5 +1,5 @@
 export type BlockType =
-    'heading' | 'heading1' | 'heading2' | 'paragraph' | 'document' | 'image' | 'video' | 'code' | 'link' | 'bullet';
+    'heading' | 'heading1' | 'heading2' | 'paragraph' | 'document' | 'image' | 'video' | 'code' | 'link' | 'bullet' | 'divider';
 
 export type TextBlockType = 'heading' | 'heading1' | 'heading2' | 'paragraph';
 
