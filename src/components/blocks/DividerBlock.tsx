26dca7f4eeafa0a1294b13eadc7a152fb60fe3d8
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-02-15 18:09:38 +0530

    feat(DividerBlock): add new DividerBlock component for enhanced layout options

---
+++ src/components/blocks/DividerBlock.tsx
@@ -0,0 +1,43 @@
+import {useRef, useEffect} from 'react';
+import { X } from "lucide-react";
+import type { Block } from "../../types/block";
+
+interface DividerBlockProps {
+    block: Block;
+    onChange: (id: string, value: string) => void;
+    onDelete?: (id: string) => void;
+    autoFocus: string | null; 
+}
+
+function DividerBlock({block, onDelete, autoFocus}: DividerBlockProps) {
+    const dividerRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(()=>{
+        if(autoFocus === block._id){
+            dividerRef.current?.focus()
+        }
+    }, [autoFocus, block._id])
+  return (
+    <>
+        <div 
+            ref={dividerRef}
+            tabIndex={-1}
+            className="relative my-6 group "
+        >
+            <hr className="border-zinc-700" />
+            <button
+                onClick={() => {
+                    if (block._id) {
+                        onDelete?.(block._id);
+                    }
+                }}
+                className="absolute -bottom-2.5 right-0 opacity-100 group-hover:opacity-100 text-red-600 hover:bg-red-100 rounded-lg"
+            >
+                <X size={20} />
+            </button>
+        </div>
+    </>
+  )
+}
+
+export default DividerBlock
