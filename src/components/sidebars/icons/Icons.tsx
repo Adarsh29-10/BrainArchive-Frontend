53d250f5e1952f8d3df57ad69aff1601e1dc6f71
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-02-15 18:42:04 +0530

    feat(Icons): add PIcon and P1Icon components for enhanced icon options

---
+++ src/components/sidebars/icons/Icons.tsx
@@ -0,0 +1,7 @@
+export const PIcon = () => (
+  <span className="font-semibold text-pink-500 inline-block">P</span>
+);
+
+export const P1Icon = () => (
+  <span className="font-semibold text-pink-500 inline-block">P1</span>
+);
