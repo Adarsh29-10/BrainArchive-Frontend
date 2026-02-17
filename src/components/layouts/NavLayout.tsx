ec5f178c0e58658fa48c7b852d57ae632d94a91a
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-02-17 23:28:50 +0530

    fix(NavLayout): update overflow properties for Outlet to improve scrolling behavior

--- src/components/layouts/NavLayout.tsx
+++ src/components/layouts/NavLayout.tsx
@@ -5,7 +5,7 @@ function NavLayout() {
   return (
     <div className="h-dvh flex flex-col overflow-hidden">
         <Navbar />
-        <div className="flex-1 min-h-0 overflow-hidden"><Outlet /></div>
+        <div className="flex-1 min-h-0 overflow-y-auto overflow-x-hidden"><Outlet /></div>
         
     </div>
   )
