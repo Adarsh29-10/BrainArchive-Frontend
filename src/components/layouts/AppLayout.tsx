a5c290393f28278533f0d8523793b1466d0c3dcc
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-02-17 22:41:56 +0530

    feat(AppLayout): restructure layout to include AppPrimaryNav for improved navigation

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../navbars/Navbar"
+import AppPrimaryNav from "../sidebars/AppPrimaryNav"
 
 function AppLayout() {
   return (
     <div className="h-dvh flex flex-col overflow-hidden">
         <Navbar />
-        <div className="flex-1 min-h-0 overflow-hidden"><Outlet /></div>
-        
+        <div className="flex-1 min-h-0 flex overflow-hidden">
+          <AppPrimaryNav />
+          <div className="flex-1 min-h-0 overflow-hidden pb-16 md:pb-0">
+            <Outlet />
+          </div>
+        </div>
     </div>
   )
 }
