3f47e4f8eb02ad10b4ba87a5620e8e75e50f62cd
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-02-20 00:34:48 +0530

    feat(EditorSidebar): add DotMD button for markdown conversion and integrate ConvertNotesFromModal

--- src/modules/editor/components/sidebars/EditorSidebar.tsx
+++ src/modules/editor/components/sidebars/EditorSidebar.tsx
@@ -2,14 +2,18 @@ import { ChevronDown } from "lucide-react"
 import {useState} from 'react';
 import type { BlockType } from "../../types";
 import { SIDEBAR_SECTIONS } from "../sidebars/SidebarPalette";
+import { DotMD } from "./icons/Icons";
+import ConvertNotesFromModal from "../../features/markdown/components/ConvertNotesFromModal";
 
 type Props = {
     addBlock: (type: BlockType) => void;
+    notebookId: string;
 }
 
-function EditorSidebar({addBlock} : Props) {
+function EditorSidebar({addBlock, notebookId} : Props) {
 
     const [expandedSections, setExpandedSections] = useState<string[]>([SIDEBAR_SECTIONS[0].title])
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const toggleSection = (title: string) => {
         setExpandedSections(prev => 
@@ -64,13 +68,33 @@ function EditorSidebar({addBlock} : Props) {
                         )}
                     </div>
                 ))}
+
+                <hr />
+                <div className="pl-4 pt-1 space-y-3 overflow-y-auto">
+                    <h2 className="text-sm font-bold text-white">Smart Features</h2>
+                    <button
+                        onClick={() => setIsModalOpen(true)}
+                        className="mx-1 px-3 py-2.5 bg-zinc-800 border-2 border-zinc-700 rounded-lg hover:border-green-500 hover:bg-green-950/30 active:scale-95 transition-all duration-200 focus:outline-none focus:border-green-500 focus:ring-2 focus:ring-green-600/30 group"
+                        title={`.md to notes converter`}
+                    >
+                        <DotMD />
+                        
+                    </button>
+                </div>
             </div>
 
             {/* Footer Info */}
             <div className="px-4 py-3 border-t border-zinc-800 bg-zinc-900 text-xs text-zinc-500 flex-shrink-0">
                 <p>💡 Tip: Organize your content with different block types for better structure.</p>
             </div>
+
+            <ConvertNotesFromModal 
+                isOpen={isModalOpen}
+                onClose={()=>setIsModalOpen(false)}
+                notebookId={notebookId}
+            />
         </aside>
+
     )
 }
 
