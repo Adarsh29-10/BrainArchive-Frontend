edb4bc08e178865802175502ee419a0fa98accd9
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-03-02 16:27:10 +0530

    fix(textBlockVariants): update margin and text size for heading and paragraph variants and add quote

--- src/modules/editor/components/blocks/textBlockVariants.ts
+++ src/modules/editor/components/blocks/textBlockVariants.ts
@@ -14,29 +14,35 @@ const BASE_TEXTAREA =
 export const TEXT_BLOCK_VARIANTS = {
 
   heading: {
-    textareaClassName: `${BASE_TEXTAREA} text-4xl font-bold mt-8`,
+    textareaClassName: `${BASE_TEXTAREA} text-4xl font-bold mt-8 mb-4`,
     placeholder: 'Heading...',
   },
 
   heading1: {
-    textareaClassName: `${BASE_TEXTAREA} text-2xl font-bold mt-4 px-0.5`,
+    textareaClassName: `${BASE_TEXTAREA} text-2xl font-bold mt-6 px-0.5`,
     placeholder: 'Heading1...',
   },
 
   heading2: {
-    textareaClassName: `${BASE_TEXTAREA} text-lg sm:text-xl font-bold mt-3 px-0.5`,
+    textareaClassName: `${BASE_TEXTAREA} text-lg sm:text-xl font-semibold mt-4 px-0.5`,
     placeholder: 'Heading2...',
   },
 
   paragraph: {
-    containerClassName: 'relative -mb-2 group select-text',
+    containerClassName: 'relative mb-2.5 group select-text',
     textareaClassName: `${BASE_TEXTAREA} text-base sm:text-lg leading-relaxed mt-1 pl-2`,
     placeholder: 'Write something...',
   },
 
   paragraph1: {
-    containerClassName: 'relative -mb-2 group select-text',
+    containerClassName: 'relative mb-2 group select-text',
     textareaClassName: `${BASE_TEXTAREA} text-lg sm:text-xl leading-relaxed mt-1 pl-2`,
     placeholder: 'Write something...',
   },
+
+  quotes: {
+    containerClassName: 'relative mb-4 group select-text border-l-2 border-green-200 ml-8 mr-12',
+    textareaClassName: `${BASE_TEXTAREA} font-semibold text-lg sm:text-xl leading-relaxed mt-1 pl-4 py-2 bg-green-50/10 `,
+    placeholder: 'Write Quotes',
+  }
 };
