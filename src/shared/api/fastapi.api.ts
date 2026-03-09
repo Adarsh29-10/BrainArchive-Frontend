9cc253c7bfbb9f131662b6eaa89d598edae76110
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-03-09 15:14:55 +0530

    fix(api): assignment for fastapiApi configuration

--- src/shared/api/fastapi.api.ts
+++ src/shared/api/fastapi.api.ts
@@ -2,7 +2,9 @@
 import axios from "axios";
 
 export const fastapiApi = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    // baseURL: "http://127.0.0.1:8000",
+    baseURL: import.meta.env.VITE_API_URL_AI,
+    withCredentials: true,
 });
 
 let getToken: (() => Promise<string>) | null = null;
