384a53890a0a84517af1e86649751ed06354c069
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-03-12 22:01:44 +0530

    refactor(Navbar): reorganize layout and improve back navigation logic

--- src/modules/app-shell/components/Navbar.tsx
+++ src/modules/app-shell/components/Navbar.tsx
@@ -1,35 +1,52 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Home } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Navbar() {
   const { user, isLoading } = useAuth0();
-
+  const {pathname} = useLocation()
   const navigate = useNavigate()
+  const hideBackButton = ['/dashboard', '/ai', '/notebooks'].includes(pathname)
 
   if (isLoading) {
     return null;
+  }
+
+  const handleBack = () => {
+    if(pathname.startsWith('/nb/')) { navigate('/dashboard')}
+    else if(pathname.startsWith('/chat/')) { navigate('/ai')}
+    else navigate(-1)
   }
 
 
   return (
     <nav className="bg-zinc-950 backdrop-blur-md border-b border-black/20 sticky top-0 z-50">
       <div className="flex justify-between items-center px-4 sm:px-8 lg:px-16 py-3">
-        
+          
         {/* Logo */}
         <div className="flex items-center gap-2">
           <span className="text-xl sm:text-2xl font-bold text-white">BrainArchive</span>
         </div>
 
-        <img
-          onClick={()=>navigate('/profile')}
-          src={user?.picture}
-          alt={user?.name}
-          className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border-4 border-zinc-900 bg-zinc-800 object-cover"
-        />
+        <div className="flex gap-4">
+
+          {!hideBackButton && <button
+              className="text-xl sm:text-xl font-semibold text-white"
+              onClick={handleBack}>
+                {<Home /> }
+            </button> 
+          }
+
+          <img
+            onClick={()=>navigate('/profile')}
+            src={user?.picture}
+            alt={user?.name}
+            className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border-4 border-zinc-900 bg-zinc-800 object-cover"
+            />
+        </div>
        
       </div>
-
-     
     </nav>
   );
 }
